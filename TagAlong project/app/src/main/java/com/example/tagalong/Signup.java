8d38ae305797a9fbdc222754c63d8e9027f4e54a
Yash_Sardhara <yash.sardhara1@gmail.com>
2019-10-19 23:30:30 -0700

    Signup fragments set

--- TagAlong project/app/src/main/java/com/example/tagalong/Signup.java
+++ TagAlong project/app/src/main/java/com/example/tagalong/Signup.java
@@ -1,7 +1,11 @@
 package com.example.tagalong;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class Signup extends AppCompatActivity {
 
@@ -9,5 +13,34 @@ public class Signup extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        BottomNavigationView btv = findViewById(R.id.menu_signup);
+        btv.setOnNavigationItemReselectedListener(lister);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_signup_container, new Signup1_Fragment()).commit();
     }
+
+    private BottomNavigationView.OnNavigationItemReselectedListener lister =
+            new BottomNavigationView.OnNavigationItemReselectedListener() {
+                @Override
+                public void onNavigationItemReselected(@NonNull MenuItem menuItem) {
+                    Fragment frag = null;
+
+                    switch (menuItem.getItemId()){
+                        case R.id.signup_one:
+                            frag = new Signup1_Fragment();
+                            break;
+
+                        case R.id.signup_two:
+                            frag = new Signup2_Fragment();
+                            break;
+
+                        case R.id.signup_submit:
+                            frag = new Signup1_Fragment();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_signup_container, frag).commit();
+                }
+            };
 }
