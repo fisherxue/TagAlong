41edbc3173b07069b2550ba38c79d0bf3cf25995
Ming Xuan Choy <iancmx@hotmail.com>
2019-10-23 00:26:23 -0700

    fasdf

--- backend/Trip/controllers/newTrip.js
+++ backend/Trip/controllers/newTrip.js
@@ -1,23 +1,77 @@
 const TripStore = require('../models/Trip');
 const User = require('../../User/models/user');
 
+const tripScheduler = require('../../router/routeScheduler');
+const directions = require('../../router/directions');
+
 const handleCreateTrip = async (req, res) => {
-	
-	const { username, routedata } = req.body;
+		
+	console.log("/newTrip hit");
+
+	console.log(req.body);
+
+	const { username, arrivaltime, tripRoute, isDriverTrip } = req.body;
 
 	const user = await User.findOne({ username });
 
 	if (user) {
-		trip = new Trip({
 
+		let bigboijson;
+
+		await directions.getDirections(tripRoute, res => {
+			bigboijson = res;
 		})
 
-		await trip.save(err => {
-			if(err)
-				res.status(400).send("ERROR MISSING FIELD")
-			else
-				res.send(trip);
-		});
+		trip = new TripStore({
+			username: username,
+			arrivaltime: arrivaltime,
+			tripRoute: bigboijson,
+			isDriverTrip: isDriverTrip
+		})
+
+		// await trip.save(err => {
+		// 	if(err)
+		// 		res.status(400).send("ERROR MISSING FIELD")
+		// 	else
+		// 		res.send(trip);
+		// });
+
+		if (isDriverTrip) {
+			await tripScheduler.routeSchedulerRequestHandler(trip.tripRoute, (res, usertrips) => {
+
+				users = [];
+				usertrips.forEach(usertrip => {
+					users.push(usertrip.username);
+				})
+
+				optimizedTrip = new TripStore({
+					username: username,
+					arrivaltime: arrivaltime,
+					tripRoute: res,
+					isDriverTrip: true,
+					ass_user: users
+				})
+
+				optimizedTrip.save(err => {
+					if(err)
+						res.status(400).send("ERROR MISSING FIELD");
+					else
+						res.send(trip);
+				})
+
+
+
+			})
+		}
+		else {
+			trip.save(err => {
+					if(err)
+						res.status(400).send("ERROR MISSING FIELD");
+					else
+						res.send(trip);
+				})
+		}
+
 	}
 	else {
 		return res.status(400).send("Incorrect email or password");
