48b7bdfaf87172427be8059f96e783096c8c255d
Ming Xuan Choy <iancmx@hotmail.com>
2019-10-30 19:19:26 -0700

    complexity fix in updateprofile 2.0

--- backend/User/controllers/updateProfile.js
+++ backend/User/controllers/updateProfile.js
@@ -7,17 +7,7 @@ const handleProfileUpdate = async (req, res) => {
 	
 	const { userID, firstName, lastName, age, gender, email, interests, isDriver, carCapacity, fbToken } = req.body;
 
-	const update = {
-		...firstName && { firstName },
-		...lastName && { lastName },
-		...age && { age },
-		...gender && { gender },
-		...email && { email },
-		...isDriver && { isDriver },
-		...email && { email },
-		...carCapacity && { carCapacity },
-		...fbToken && { fbToken }
-	};
+	const update = createUpdateObject(req);
 
 	if (mongoose.Types.ObjectId.isValid(userID)) {
 
@@ -39,6 +29,26 @@ const handleProfileUpdate = async (req, res) => {
 	
 
 };
+
+const createUpdateObject = (req) => {
+
+	const { userID, firstName, lastName, age, gender, email, interests, isDriver, carCapacity, fbToken } = req.body;
+
+	const update = {
+		...firstName && { firstName },
+		...lastName && { lastName },
+		...age && { age },
+		...gender && { gender },
+		...email && { email },
+		...isDriver && { isDriver },
+		...email && { email },
+		...carCapacity && { carCapacity },
+		...fbToken && { fbToken }
+	};
+
+	return update;
+}
+
 
 module.exports = {
 	handleProfileUpdate
