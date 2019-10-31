157dcadc387612f9fe0e137e05f9e4fa467d1092
Ming Xuan Choy <iancmx@hotmail.com>
2019-10-31 13:11:54 -0700

    fixed mongoose schema naming

--- backend/Trip/models/Trip.js
+++ backend/Trip/models/Trip.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const ObjectId = mongoose.Schema.Types.ObjectId;
 
 const TripSchema = mongoose.Schema({
-	arrivaltime: {
+	arrivalTime: {
 		type: Date,
 		required: true
 	},
