09405a29316048773190fffd28fffe5a878454eb
Yash_Sardhara <yash.sardhara1@gmail.com>
2019-10-30 20:08:33 -0700

    fixed more styling issues

--- Front End Oreo Version/app/src/main/java/com/tagalong/tagalong/FirebaseMessagingServiceHandler.java
+++ Front End Oreo Version/app/src/main/java/com/tagalong/tagalong/FirebaseMessagingServiceHandler.java
@@ -16,16 +16,7 @@
 
 package com.tagalong.tagalong;
 
-/*import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Context;
-import android.content.Intent;
-import android.media.RingtoneManager;
-import android.net.Uri;
-import android.os.Build;*/
 import android.util.Log;
-
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
