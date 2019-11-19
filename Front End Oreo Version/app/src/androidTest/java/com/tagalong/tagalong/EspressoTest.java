f08e42040987b0ec7f3a3bca2ebf32224884508f
Yash_Sardhara <yash.sardhara1@gmail.com>
2019-11-18 22:53:56 -0800

    signup recorded test

--- Front End Oreo Version/app/src/androidTest/java/com/tagalong/tagalong/EspressoTest.java
+++ Front End Oreo Version/app/src/androidTest/java/com/tagalong/tagalong/EspressoTest.java
@@ -43,45 +43,6 @@ import static org.junit.Assert.assertEquals;
 public class EspressoTest {
     @Rule
     public ActivityTestRule<MainActivity> mainActivityActivityTestRule = new ActivityTestRule<>(MainActivity.class);
-
-    @Before
-    public void setUp() throws Exception {
-        mainActivityActivityTestRule.getActivity();
-    }
-
-    @Test
-    public void testSignUp() {
-        onView(withId(R.id.signup_button)).perform(click());
-        onView(withId(R.id.signup_button)).check(matches(isDisplayed()));
-
-        /*
-        onView(withId(R.id.username)).perform(typeText("wong12"));
-        onView(withId(R.id.password)).perform(typeText("xd"));
-        onView(withId(R.id.password2)).perform(typeText("xd"));
-        closeSoftKeyboard();
-        onView(withId(R.id.Email)).perform(typeText("chunru77@byronz.com"));
-        closeSoftKeyboard();
-        onView(withId(R.id.nextbutton)).perform(click());
-        onView(withId(R.id.nextbutton)).check(matches(isDisplayed()));
-
-        onView(withHint("First Name")).perform(typeText("Chun Ru"));
-        onView(withId(R.id.lastName)).perform(typeText("Wong"));
-        onView(withId(R.id.gender)).perform(typeText("Female"));
-        onView(withId(R.id.age)).perform(typeText("96"));
-        onView(withId(R.id.isDriver)).perform(click());
-        onView(withId(R.id.seekMusic)).perform(swipeRight());
-        onView(withId(R.id.seekSpeed)).perform(swipeRight());
-        onView(withId(R.id.seekSmoking)).perform(swipeLeft());
-        onView(withId(R.id.seekFragrance)).perform(swipeRight());
-        onView(withId(R.id.seekChatting)).perform(swipeLeft());
-
-         */
-        //onView(withId(R.id.submit)).perform(click());
-        //onView(withId(R.id.nav_chat)).perform(click());
-        //onView(withText("Successfully Logged In")).check(matches(isDisplayed()));
-        //onView(withId(R.id.bottom_navigation)).check(matches(isDisplayed()));
-
-    }
     @Test
     public void testChat() {
         onView(withId(R.id.userNameLogin)).perform(typeText("bwong"));
@@ -93,37 +54,7 @@ public class EspressoTest {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        //onView(ViewMatchers.withId(R.id.home_frag_recycler_view)).perform(RecyclerViewActions.actionOnItemAtPosition(1, ))
-
+        onView(ViewMatchers.withId(R.id.home_frag_recycler_view)).perform(RecyclerViewActions.actionOnItemAtPosition(1, click()));
+        onView(ViewMatchers.withId(R.id.chat)).perform(click());
     }
-
-    @Test
-    public void testLogin() {
-        onView(withId(R.id.userNameLogin)).perform(typeText("bwong"));
-        onView(withId(R.id.passwordLogin)).perform(typeText("xd"));
-        closeSoftKeyboard();
-        onView(withId(R.id.login_button)).perform(click());
-        pressBack();
-        //onView(withId(R.id.bottom_navigation)).check(matches(isDisplayed()));
-        /*
-        onView(withId(R.id.bottom_navigation)).check(matches(isDisplayed()));
-        onView(withId(R.id.edit)).perform(click());
-        onView(withId(R.id.firstName)).perform(typeText("Chun Ru"));
-        onView(withId(R.id.lastName)).perform(typeText("Wong"));
-        onView(withId(R.id.gender)).perform(typeText("Male"));
-        onView(withId(R.id.age)).perform(typeText("69"));
-        onView(withId(R.id.carCapacity)).perform(typeText("0"));
-        onView(withId(R.id.seekMusic)).perform(swipeRight());
-        onView(withId(R.id.seekSpeed)).perform(swipeRight());
-        onView(withId(R.id.seekSmoking)).perform(swipeLeft());
-        onView(withId(R.id.seekFragrance)).perform(swipeRight());
-        onView(withId(R.id.seekChatting)).perform(swipeLeft());
-        onView(withId(R.id.submit)).perform(click());
-        onView(withText("YOUR PROFILE")).check(matches(isDisplayed()));
-
-
-         */
-    }
-
-
 }
