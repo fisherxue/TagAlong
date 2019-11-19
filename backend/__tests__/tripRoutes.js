d2ab3e15299d8ff4dad5707ced5c55ce34645db1
Ming Xuan Choy <iancmx@hotmail.com>
2019-11-19 01:51:55 -0800

    uncomment

--- backend/__tests__/tripRoutes.js
+++ backend/__tests__/tripRoutes.js
@@ -58,7 +58,6 @@ afterAll(async () => {
 })
 
 describe('testing trips', () => {
-    /*
     it('should create a new rider trip', async (done) => {
         user1 = new User({
             username: "chatdemouser13",
@@ -214,99 +213,98 @@ describe('testing trips', () => {
 
         done();
     })
-    */
 
-    // it('new driver trip should match with 3 other user trips', async (done) => {
+    it('new driver trip should match with 3 other user trips', async (done) => {
         
-    //     user1 = new User({
-    //         _id: "5dd36a75b458db53e031818e",
-    //         username: "bwong",
-    //         email: "bwong@demo.com",
-    //         password: "demodemodemo",
-    //         fbToken: "",
-    //         isDriverTrip: false,
-    //         interests: [5,5,5,5,5]
-    //     });
+        user1 = new User({
+            _id: "5dd36a75b458db53e031818e",
+            username: "bwong",
+            email: "bwong@demo.com",
+            password: "demodemodemo",
+            fbToken: "",
+            isDriverTrip: false,
+            interests: [5,5,5,5,5]
+        });
 
-    //     await user1.save();
+        await user1.save();
 
-    //     user2 = new User({
-    //         _id: "5dd36c81b458db53e031819b",
-    //         username: "bwong5",
-    //         email: "bwong5@demo.com",
-    //         password: "demodemodemo",
-    //         fbToken: "",
-    //         isDriverTrip: false,
-    //         interests: [5,5,5,5,5]
+        user2 = new User({
+            _id: "5dd36c81b458db53e031819b",
+            username: "bwong5",
+            email: "bwong5@demo.com",
+            password: "demodemodemo",
+            fbToken: "",
+            isDriverTrip: false,
+            interests: [5,5,5,5,5]
 
-    //     });
+        });
 
-    //     await user2.save();
+        await user2.save();
 
-    //     user3 = new User({
-    //         _id: "5dd36b0fb458db53e0318192",
-    //         username: "bwong2",
-    //         email: "bwong2@demo.com",
-    //         password: "demodemodemo",
-    //         fbToken: "",
-    //         isDriverTrip: false,
-    //         interests: [5,5,5,5,5]
+        user3 = new User({
+            _id: "5dd36b0fb458db53e0318192",
+            username: "bwong2",
+            email: "bwong2@demo.com",
+            password: "demodemodemo",
+            fbToken: "",
+            isDriverTrip: false,
+            interests: [5,5,5,5,5]
 
-    //     });
+        });
 
-    //     await user3.save();
+        await user3.save();
 
-    //     let driver;
+        let driver;
 
-    //     user4 = new User({
-    //         username: "bwong1",
-    //         email: "bwong1@demo.com",
-    //         password: "demodemodemo",
-    //         fbToken: "",
-    //         isDriverTrip: true,
-    //         interests: [5,5,5,5,5]
-    //     });
+        user4 = new User({
+            username: "bwong1",
+            email: "bwong1@demo.com",
+            password: "demodemodemo",
+            fbToken: "",
+            isDriverTrip: true,
+            interests: [5,5,5,5,5]
+        });
 
-    //     await user4.save().then((user) => {
-    //         driver = user;
-    //     });
-
-
-    //     riderTrip1 = new TripStore(ridertrip1);
-    //     await riderTrip1.save();
-    //     riderTrip2 = new TripStore(ridertrip2);
-    //     await riderTrip2.save();
-    //     riderTrip3 = new TripStore(ridertrip3);
-    //     await riderTrip3.save();
-
-    //     const res = await request.post("/trips/newTrip")
-    //         .send({
-    //            "arrivalPlace":"not assigned",
-    //            "arrivalTime":"Nov 26, 2019 10:20:00",
-    //            "departurePlace":"not assigned",
-    //            "departureTime":"Nov 19, 2019 00:42:59",
-    //            "isDriverTrip":true,
-    //            "roomID":"not assigned",
-    //            "taggedUsers":[
-    //               "not assigned"
-    //            ],
-    //            "tripID":"not assigned",
-    //            "tripRoute":{
-    //               "nameValuePairs":{
-    //                  "origin":"49.262158,-123.2500847",
-    //                  "destination":"49.28284681397363,-123.12324184924364"
-    //               }
-    //            },
-    //            "userID": driver._id,
-    //            "username": driver.username
-    //         }).expect(200);
-
-    //     expect(res.body.taggedUsers).toEqual(expect.arrayContaining(["bwong", "bwong5", "bwong2"]));
+        await user4.save().then((user) => {
+            driver = user;
+        });
 
 
-    //     done();
+        riderTrip1 = new TripStore(ridertrip1);
+        await riderTrip1.save();
+        riderTrip2 = new TripStore(ridertrip2);
+        await riderTrip2.save();
+        riderTrip3 = new TripStore(ridertrip3);
+        await riderTrip3.save();
 
-    // })
+        const res = await request.post("/trips/newTrip")
+            .send({
+               "arrivalPlace":"not assigned",
+               "arrivalTime":"Nov 26, 2019 10:20:00",
+               "departurePlace":"not assigned",
+               "departureTime":"Nov 19, 2019 00:42:59",
+               "isDriverTrip":true,
+               "roomID":"not assigned",
+               "taggedUsers":[
+                  "not assigned"
+               ],
+               "tripID":"not assigned",
+               "tripRoute":{
+                  "nameValuePairs":{
+                     "origin":"49.262158,-123.2500847",
+                     "destination":"49.28284681397363,-123.12324184924364"
+                  }
+               },
+               "userID": driver._id,
+               "username": driver.username
+            }).expect(200);
+
+        expect(res.body.taggedUsers).toEqual(expect.arrayContaining(["bwong", "bwong5", "bwong2"]));
+
+
+        done();
+
+    })
 
 
     it('gettrips for valid user', async (done) => {
