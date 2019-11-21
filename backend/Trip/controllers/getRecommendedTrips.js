eac839a72d08c8f75f89a1da4492dd862d796357
Ming Xuan Choy <iancmx@hotmail.com>
2019-11-20 22:22:27 -0800

    fix return trip array

--- backend/Trip/controllers/getRecommendedTrips.js
+++ backend/Trip/controllers/getRecommendedTrips.js
@@ -1,17 +1,22 @@
 const TripStore = require("../models/Trip");
 const mongoose = require("mongoose");
 const tripRecommender = require("../../triprecommender/recommender");
+const debug = require("debug")("http /getRecommendedTrips");
 
 
 const handleGetRecommendedTrips = async (req, res) => {
 
+	debug("/getRecommendedTrips hit");
+
 	const tripID = req.headers.tripid;
+
+	debug("tripID" , tripID);
 
 	if (mongoose.Types.ObjectId.isValid(tripID)) {
 		await TripStore.findById(tripID, (err, trip) => {
 			if (trip) {
 				tripRecommender.driverTripHandler(trip, function(riderTrips, driverTrip) {
-					res.json(riderTrips);
+					res.json({ trips: riderTrips });
 				});
 			} else {
 				res.status(400).send("trip not found");
