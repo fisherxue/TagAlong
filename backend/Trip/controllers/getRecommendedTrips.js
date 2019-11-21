b3e20f19654f2e0419ce400b00ad2c3778baaba8
Ming Xuan Choy <iancmx@hotmail.com>
2019-11-20 22:56:46 -0800

    modify get recommended trips

--- backend/Trip/controllers/getRecommendedTrips.js
+++ backend/Trip/controllers/getRecommendedTrips.js
@@ -2,29 +2,48 @@ const TripStore = require("../models/Trip");
 const mongoose = require("mongoose");
 const tripRecommender = require("../../triprecommender/recommender");
 const debug = require("debug")("http /getRecommendedTrips");
+const User = require("../../User/models/user");
 
 
 const handleGetRecommendedTrips = async (req, res) => {
 
 	debug("/getRecommendedTrips hit");
 
-	const tripID = req.headers.tripid;
+	const userID = req.headers.userid;
 
-	debug("tripID" , tripID);
+	if (mongoose.Types.ObjectId.isValid(userID)) {
+		User.findById(userID, (err, user) => {
+			if (user) {
+				if (user.isDriver) {
+					TripStore.find({ userID }, (err, trips) => {
+						let recommendedTrips = [];
+						trips.forEach(trip => {
+							let appendingobj = {
+								drivertrip: {}
+								riderTrips: []
+							};
 
-	if (mongoose.Types.ObjectId.isValid(tripID)) {
-		await TripStore.findById(tripID, (err, trip) => {
-			if (trip) {
-				tripRecommender.driverTripHandler(trip, function(riderTrips, driverTrip) {
-					res.json({trips: riderTrips});
-				});
+							appendingobj.drivertrip = trip;
+							tripRecommender.driverTripHandler(trip, function(riderTrips, driverTrip) {
+								appendingobj.riderTrips = riderTrips;
+							});
+						})
+
+						debug("responing recommended trips", appendingobj);
+						res.json(appendingobj);
+					})
+				} else {
+					res.status(400).send("User is not a driver");
+				}
+
+				
 			} else {
-				res.status(400).send("trip not found");
+				res.status(400).send("Unable to find user");
 			}
-
 		});
 	} else {
-		return res.status(400).send("Invalid userID");
+		debug("invalid userID");
+		res.status(400).send("Invalid userID");
 	}
 
 };
