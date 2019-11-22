c21ced9a3d157e8f975e981a8c2f947041e0c24e
Ubuntu <ubuntu@ip-172-31-24-210.ec2.internal>
2019-11-22 05:00:15 +0000

    fixed accept trip

--- backend/Trip/controllers/acceptTrip.js
+++ backend/Trip/controllers/acceptTrip.js
@@ -105,6 +105,8 @@ const handleAcceptTrip = async (req, res) => {
 		driverTrip.save();
 		riderTrip.drivertripID = driverTrip._id;
 		riderTrip.chatroomID = driverTrip.chatroomID;
+		riderTrip.isFulfilled = true;
+		riderTrip.taggedUsers.push(driverTrip.username);
 		riderTrip.save();
 
 		addUsertoChatRoom(rider_user.username, driverTrip.chatroomID);
