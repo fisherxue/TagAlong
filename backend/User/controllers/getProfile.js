9984435376bc71e9bccefee2c3c86c1c6f467995
Ming Xuan Choy <iancmx@hotmail.com>
2019-11-21 21:06:04 -0800

    added get profile

---
+++ backend/User/controllers/getProfile.js
@@ -0,0 +1,23 @@
+const User = require("../../User/models/user");
+const mongoose = require("mongoose");
+const debug = require("debug")("http /getProfile");
+
+const handleGetProfile = async (req, res) => {
+	debug("/getProfile");
+	debug(req.headers);
+
+	const username = req.header.username;
+
+	const user = await User.findOne({ username });
+
+	if (!user) {
+		return res.status(400).send("User not found");
+	}
+
+	res.send(user);
+
+};
+
+module.exports = {
+	handleGetProfile;
+};
