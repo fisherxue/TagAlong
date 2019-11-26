d0a8adb5d92221c5a149bdaef7590274fc35a82c
Ming Xuan Choy <iancmx@hotmail.com>
2019-11-25 22:21:08 -0800

    add logout route

---
+++ backend/User/controllers/logout.js
@@ -0,0 +1,35 @@
+const User = require("../models/user");
+const mongoose = require("mongoose");
+const debug = require("debug")("http /logout");
+
+
+/**
+ * handleProfileUpdate: Checks the given userID and updates the user corresponding
+ * 						to the userID with the new fields provided.
+ */ 
+
+const handleLogout = async (req, res) => {
+
+	debug("/logout hit");
+	debug(req.body);
+	
+	const { userID, fbToken } = req.body;
+
+	if (!mongoose.Types.ObjectId.isValid(userID)) {
+		debug("Invalid userID");
+		return res.status(400).send("Invalid userID");
+	}
+
+	const updateduser = await User.findByIdAndUpdate(userID, fbToken, {new: true});
+
+	debug("updated user", updateduser);
+	res.json(updateduser);
+
+};
+
+
+
+
+module.exports = {
+	handleLogout
+};
